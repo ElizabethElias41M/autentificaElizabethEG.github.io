01af3c47a1dd7b2f48028a5aa8b1628b00e20797
ElizabethElias41M <81278374+ElizabethElias41M@users.noreply.github.com>
2021-03-23 23:07:35 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,13 +2,13 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-   var firebaseConfig = {
-    apiKey: "AIzaSyDgLMRhMHqP9RghnG-C686mMdCR6CS1TuI",
+  apiKey: "AIzaSyDgLMRhMHqP9RghnG-C686mMdCR6CS1TuI",
     authDomain: "autentificae.firebaseapp.com",
     projectId: "autentificae",
     storageBucket: "autentificae.appspot.com",
     messagingSenderId: "618976095988",
     appId: "1:618976095988:web:4875d7ea91c618abe9f8a1",
     measurementId: "G-98C3SMT4GT"
+
 
     });
